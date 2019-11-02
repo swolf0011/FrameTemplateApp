e7cad2e226b373b6a31579e893471ff9bcbb49a8
swolf0011 <a123456>
2019-11-02 20:57:30 +0800

    update files

--- app/src/main/java/com/swolf/ly/frametemplateapp/LauncherActivity.kt
+++ app/src/main/java/com/swolf/ly/frametemplateapp/LauncherActivity.kt
@@ -8,12 +8,12 @@ import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.swolf.ly.common.ARouterAddress
 import com.swolf.ly.common.ARouterInjectable
-
-class MainActivity : AppCompatActivity(), ARouterInjectable {
+@Route(path= ARouterAddress.App_LauncherActivity)
+class LauncherActivity : AppCompatActivity(), ARouterInjectable {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_launcher)
     }
 
     fun onClickLogin(v:View){
--- lib_common/src/main/java/com/swolf/ly/common/ARouterAddress.kt
+++ lib_common/src/main/java/com/swolf/ly/common/ARouterAddress.kt
@@ -2,11 +2,12 @@ package com.swolf.ly.common
 
 
 object ARouterAddress {
-    const val Login_LoginActivity="/Login/LoginActivity"
-    const val Login_RegisterActivity="/Login/RegisterActivity"
-    const val Main_MainActivity="/Main/MainActivity"
-    const val Main_HomeActivity="/Main/HomeActivity"
-    const val Message_MessageActivity="/Message/MessageActivity"
+    const val App_LauncherActivity = "/App/LauncherActivity"
+    const val Login_LoginActivity = "/Login/LoginActivity"
+    const val Login_RegisterActivity = "/Login/RegisterActivity"
+    const val Main_MainActivity = "/Main/MainActivity"
+    const val Main_HomeActivity = "/Main/HomeActivity"
+    const val Message_MessageActivity = "/Message/MessageActivity"
 
 
 }
